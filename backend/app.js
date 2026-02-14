d3fcdd0492335b1c3c45562a312e802d1346e809
Satyam Bhambid <113852269+FameILY@users.noreply.github.com>
2026-02-14 18:45:29 +0530

    initialization

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is bhaaging 🏃‍♂️🏃‍♂️🏃‍♂️');
+});
+
+
+app.listen(process.env.port, () => {
+  console.log(`Server is running on port ${process.env.port}`);
+});
